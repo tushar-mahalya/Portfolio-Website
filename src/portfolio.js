0151c006b972c4d2616f81a2b3fb1cc097a102af
Tushar Sharma <tusharmahalya@gmail.com>
2022-11-29 00:22:57 +0530

    debugging commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -677,6 +677,13 @@ const contactPageData = {
     description:
       "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
   },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
+    link: "https://ashutoshhathidara.wordpress.com",
+    avatar_image_path: "blogs_image.svg",
+  },
   addressSection: {
     title: "Address",
     subtitle:

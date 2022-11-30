2fd62fd8779bd93499d40f58ba93c153db76a5f0
Tushar Sharma <tusharmahalya@gmail.com>
2022-11-30 16:23:13 +0530

    Update ContactComponent.js

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -11,7 +11,7 @@ import "./ContactComponent.css";
 import { greeting, contactPageData } from "../../portfolio.js";
 
 const ContactData = contactPageData.contactSection;
-const blogSection = contactPageData.blogSection;
+//const blogSection = contactPageData.blogSection;
 const addressSection = contactPageData.addressSection;
 const phoneSection = contactPageData.phoneSection;
 
